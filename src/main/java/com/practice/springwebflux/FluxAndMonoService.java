b09276a4497b63e7616c076f4a4888a6b8430e41
Ramcharan <ramcharan.adapa@cdk.com>
2021-12-02 01:08:21 +0530

    01/12/2021 - Flux with Map, FlatMap, ConcatMap. Mono with FlatMap

--- src/main/java/com/practice/springwebflux/FluxAndMonoService.java
+++ src/main/java/com/practice/springwebflux/FluxAndMonoService.java
@@ -1,6 +1,11 @@
 package com.practice.springwebflux;
 
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import lombok.var;
+import org.springframework.util.CollectionUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -21,7 +26,65 @@ public class FluxAndMonoService {
     return Flux.fromIterable(Arrays.asList("SpringBoot", "SpringData", "SpringWebFlux")).log();
   }
 
+  /**
+   * Reactive streams are immutable
+   */
+  public Flux<String> techStackFluxImmutable() {
+    var stack = Flux.fromIterable(Arrays.asList("SpringBoot", "SpringData", "SpringWebFlux")).log();
+    stack.map(String::toUpperCase);
+    return stack;
+  }
+
+  /**
+   * flatMap() - transform one source to Flux of 1 or 1 to n elements
+   * "Spring" -> Flux.just("S", "p", "r", "i", "n", "g")
+   */
+  public Flux<String> techStackFluxFlatMap() {
+    return Flux.fromIterable(Arrays.asList("Spring", "Boot"))
+        .flatMap(this::splitString)
+        .log();
+  }
+
+  public Flux<String> techStackFluxFlatMapAsync() {
+    return Flux.fromIterable(Arrays.asList("Spring", "Boot"))
+        .flatMap(this::splitStringWithDelay)
+        .log();
+  }
+
+  /**
+   * similar to flatMap but preserves the sequence of reactive stream
+   */
+  public Flux<String> techStackFluxConcatMap() {
+    return Flux.fromIterable(Arrays.asList("Spring", "Boot"))
+        .concatMap(this::splitStringWithDelay)
+        .log();
+  }
+
+  public Flux<String> splitStringWithDelay(String str) {
+    var charArray = str.split("");
+    var delay = new Random().nextInt(1000);
+    return Flux.fromArray(charArray)
+        .delayElements(Duration.ofMillis(delay));
+  }
+
+  public Flux<String> splitString(String str) {
+    var charArray = str.split("");
+    return Flux.fromArray(charArray);
+  }
+
   public Mono<String> techStackMono() {
     return Mono.just("SpringWebFlux").log();
+  }
+
+  public Mono<List<String>> techStackMonoFlatMap() {
+    return Mono.just("SpringWebFlux")
+        .flatMap(this::splitMono)
+        .log();
+  }
+
+  private Mono<List<String>> splitMono(String str) {
+    String[] charArray = str.split("");
+    List<String> charList = Arrays.asList(charArray);
+    return Mono.just(charList);
   }
 }
--- src/test/java/com/practice/springwebflux/FluxAndMonoServiceTest.java
+++ src/test/java/com/practice/springwebflux/FluxAndMonoServiceTest.java
@@ -1,5 +1,6 @@
 package com.practice.springwebflux;
 
+import java.util.Arrays;
 import lombok.var;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,7 +18,53 @@ public class FluxAndMonoServiceTest {
   public void testTechStackFlux() {
     var flux = fluxAndMonoService.techStackFlux();
     StepVerifier.create(flux)
+//        .expectNext("SpringBoot", "SpringData", "SpringWebFlux")
+        .expectNextCount(3)
+        .verifyComplete();
+
+  }
+
+  @Test
+  public void testTechStackFluxImmutable() {
+    var flux = fluxAndMonoService.techStackFluxImmutable();
+    StepVerifier.create(flux)
         .expectNext("SpringBoot", "SpringData", "SpringWebFlux")
+        // doesn't work as reactive streams are immutable
+//        .expectNext("SPRINGBOOT", "SPRINGDATA", "SPRINGWEBFLUX")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testTechStackFluxFlatMap() {
+    var flux = fluxAndMonoService.techStackFluxFlatMap();
+    StepVerifier.create(flux)
+        .expectNext("S", "p", "r", "i", "n", "g", "B", "o", "o", "t")
+        .verifyComplete();
+  }
+
+  @Test
+  public void testTechStackFluxFlatMapWithAsync() {
+    var flux = fluxAndMonoService.techStackFluxFlatMapAsync();
+    StepVerifier.create(flux)
+//        .expectNext("S", "p", "r", "i", "n", "g", "B", "o", "o", "t")
+        .expectNextCount(10)
+        .verifyComplete();
+  }
+
+  @Test
+  public void testTechStackFluxConcatMap() {
+    var flux = fluxAndMonoService.techStackFluxConcatMap();
+    StepVerifier.create(flux)
+        .expectNext("S", "p", "r", "i", "n", "g", "B", "o", "o", "t")
+//        .expectNextCount(10)
+        .verifyComplete();
+  }
+
+  @Test
+  public void testTechStackMonoFlatMap() {
+    var flux = fluxAndMonoService.techStackMonoFlatMap();
+    StepVerifier.create(flux)
+        .expectNext(Arrays.asList("S", "p", "r", "i", "n", "g", "W", "e", "b", "F", "l", "u", "x"))
         .verifyComplete();
   }
 }
